ea0796621063c9c51759f00b5da1e9c2c8f85359
Apurva Ojas <apurvaojas10@gmail.com>
2021-12-02 11:49:45 +0530

    Middleware

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,43 +1,59 @@
 import {
   FETCH_CHARACTERS,
   RECEIVED_CHARACTERS_ERROR,
-  RECEIVED_CHARACTERS_SUCCESS
+  RECEIVED_CHARACTERS_SUCCESS,
 } from "./actionTypes";
-
+import isPromise from "is-promise";
 
 import axios from "axios";
 
 export const fetchCharacters = () => ({
-  type: FETCH_CHARACTERS
+  type: FETCH_CHARACTERS,
 });
 
-export const receivedCharactersSuccess = payload => ({
+export const receivedCharactersSuccess = (payload) => ({
   type: RECEIVED_CHARACTERS_SUCCESS,
-  payload
+  payload,
 });
-export const receivedCharactersError = error => ({
+export const receivedCharactersError = (error) => ({
   type: RECEIVED_CHARACTERS_ERROR,
-  error
+  error,
 });
 
+export function loaderHandler({ dispatch }) {
+  console.log('test...1')
+  return (next) => {
+    console.log('test...2')
 
+    return (action) => {
+      console.log('test...3')
+
+      if (isPromise(action)) {
+        dispatch({ type: "SHOW_LOADER" });
+        action
+          .then(() => dispatch({ type: "HIDE_LOADER" }))
+          .catch(() => dispatch({ type: "HIDE_LOADER" }));
+      }
+      return next(action);
+    };
+  };
+}
 
 export function fetchPosts() {
-  return function(dispatch) {
+  return function (dispatch) {
     dispatch(fetchCharacters());
     return axios
       .get(`https://rickandmortyapi.com/api/character/`)
       .then(
-        response => response.data.results,
-        error => console.log("An error occurred.", error)
+        (response) => response.data.results,
+        (error) => console.log("An error occurred.", error)
       )
-      .then(json => {
+      .then((json) => {
         // dispatch(setFilterOptions(getFilterOptions(json)));
         dispatch(receivedCharactersSuccess(json));
       })
-      .catch(error => {
+      .catch((error) => {
         dispatch(receivedCharactersError(error));
       });
   };
 }
-
